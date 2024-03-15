cd6a02b864882904086af8a2bbab32d1c922d43c
leoli <jafee201153@gmail.com>
2024-03-15 19:11:41 +0800

    ci: add update download links plugin

--- docs/download_links.mjs
+++ docs/download_links.mjs
@@ -1,12 +1,35 @@
 import fs from 'fs';
 import https from 'node:https';
 
+// 生成 Markdown 內容並更新 README.md 檔案
 function generateMarkdown({ beforeMarker, afterMarker, content }) {
     let readme = fs.readFileSync('README.md', 'utf8');
     let before = readme.substring(0, readme.indexOf(beforeMarker) + beforeMarker.length);
     let after = readme.substring(readme.indexOf(afterMarker));
 
-    fs.writeFileSync('README.md', `${before}\n\n${content}\n${after}`);
+    fs.writeFileSync('README.md', `${before}\n${content}\n${after}`);
+}
+
+// 根據資產名稱和下載 URL 生成對應的 shell 命令
+function generateShellCommand(asset) {
+    if (asset.name.includes('win-x64')) {
+        return `powershell.exe -Command "Invoke-WebRequest -Uri "${asset.browser_download_url}" -OutFile "${asset.name}"; Start-Process "${asset.name}" -Wait; Remove-Item "${asset.name}" -Force"`;
+    } else {
+        return `curl -L "${asset.browser_download_url}" -o ${asset.name} && chmod +x ${asset.name} && ./${asset.name} && rm -f ${asset.name}`;
+    }
+}
+
+// 根據資產名稱選擇對應的標記
+function selectMarkers(asset) {
+    if (asset.name.includes('env-setup-win-x64')) {
+        return { beforeMarker: '<!-- WINDOWS_LINK_X64_START -->', afterMarker: '<!-- WINDOWS_LINK_X64_END -->' };
+    } else if (asset.name.includes('env-setup-macos-x64')) {
+        return { beforeMarker: '<!-- MACOS_LINK_X64_START -->', afterMarker: '<!-- MACOS_LINK_X64_END -->' };
+    } else if (asset.name.includes('env-setup-macos-arm64')) {
+        return { beforeMarker: '<!-- MACOS_LINK_ARM64_START -->', afterMarker: '<!-- MACOS_LINK_ARM64_END -->' };
+    } else if (asset.name.includes('env-setup-linux-x64')) {
+        return { beforeMarker: '<!-- LINUX_LINK_X64_START -->', afterMarker: '<!-- LINUX_LINK_X64_END -->' };
+    }
 }
 
 const url = `https://api.github.com/repos/DinosauriaLab/npm-env-setup/releases/latest`;
@@ -29,44 +52,13 @@ https
             release.assets.forEach((asset) => {
                 console.log(`Download URL: ${asset.browser_download_url}`);
                 console.log(`Asset name: ${asset.name}`);
-                if (asset.name.includes('env-setup-win-x64')) {
+                const shellCommand = generateShellCommand(asset);
+                const { beforeMarker, afterMarker } = selectMarkers(asset);
+                if (beforeMarker && afterMarker) {
                     generateMarkdown({
-                        beforeMarker: '<!-- WINDOWS_LINK_X64_START -->',
-                        afterMarker: '<!-- WINDOWS_LINK_X64_END -->',
-                        content: '```shell\n' + `powershell.exe -Command "Invoke-WebRequest -Uri "${asset.browser_download_url}" -OutFile "${asset.name}"; Start-Process "${asset.name}" -Wait; Remove-Item "${asset.name}" -Force"` + '\n```',
-                    });
-                }
-                if (asset.name.includes('env-setup-macos-x64')) {
-                    generateMarkdown({
-                        beforeMarker: '<!-- MACOS_LINK_X64_START -->',
-                        afterMarker: '<!-- MACOS_LINK_X64_END -->',
-                        content:
-                            '```shell\n' +
-                            `curl -L "${asset.browser_download_url}" -o "${asset.name}" && chmod +x "${asset.name}" && ./"${asset.name}" && rm -f "${asset.name}"
-                        ` +
-                            '\n```',
-                    });
-                }
-                if (asset.name.includes('env-setup-macos-arm64')) {
-                    generateMarkdown({
-                        beforeMarker: '<!-- MACOS_LINK_ARM64_START -->',
-                        afterMarker: '<!-- MACOS_LINK_ARM64_END -->',
-                        content:
-                            '```shell\n' +
-                            `curl -L "${asset.browser_download_url}" -o "${asset.name}" && chmod +x "${asset.name}" && ./"${asset.name}" && rm -f "${asset.name}"
-                        ` +
-                            '\n```',
-                    });
-                }
-                if (asset.name.includes('env-setup-linux-x64')) {
-                    generateMarkdown({
-                        beforeMarker: '<!-- LINUX_LINK_X64_START -->',
-                        afterMarker: '<!-- LINUX_LINK_X64_END -->',
-                        content:
-                            '```shell\n' +
-                            `curl -L "${asset.browser_download_url}" -o "${asset.name}" && chmod +x "${asset.name}" && ./"${asset.name}" && rm -f "${asset.name}"
-                        ` +
-                            '\n```',
+                        beforeMarker,
+                        afterMarker,
+                        content: '```shell\n' + shellCommand + '\n```',
                     });
                 }
             });
