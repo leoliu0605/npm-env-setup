41a1d292198943795886533f214b3506ef4a4ed8
leoli <jafee201153@gmail.com>
2024-07-25 13:32:48 +0800

    chore(macsetup.sh): add commands to hide hidden files in Finder and allow apps from anywhere for improved user experience

--- scripts/macos/macsetup.sh
+++ scripts/macos/macsetup.sh
@@ -12,3 +12,13 @@ defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 # Show path bar in Finder
 # //////////////////////////////////////////////////
 defaults write com.apple.finder ShowPathbar -bool true
+
+# DON'T Show hidden files in Finder
+# //////////////////////////////////////////////////
+defaults write com.apple.finder AppleShowAllFiles -bool false
+
+killall Finder
+
+# Allow apps from anywhere
+# //////////////////////////////////////////////////
+sudo spctl --master-disable
